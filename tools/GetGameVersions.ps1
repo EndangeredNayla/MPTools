5e0e5897301c38474fbb8737d4b8dbe204c8997f
Nora <norahanegan@gmail.com>
2021-08-17 15:36:26 -0400

    Update GetGameVersions.ps1

--- tools/GetGameVersions.ps1
+++ tools/GetGameVersions.ps1
@@ -1,4 +1,5 @@
 Set-Location ".."
-. ./settings.ps1
+. .\settings.ps1
+. .\secrets.ps1
 
 curl -H X-Api-Token:$CURSEFORGE_TOKEN https://minecraft.curseforge.com/api/game/versions >> GameVersions.json
