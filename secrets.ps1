52c236da4ad4c1de4744006fb49c62fbd4f892f1
Nayla Hanegan <naylahanegan@gmail.com>
2023-02-14 21:20:41 -0500

    add curseforge key to secrets

--- secrets.ps1
+++ secrets.ps1
@@ -5,16 +5,7 @@
 #  CURSEFORGE ACCOUNT SETTINGS
 # =====================================================================//
 # Upload API: https://www.curseforge.com/account/api-tokens
-
 $CURSEFORGE_UPLOADAPI_TOKEN = "My-Token"
 
-
-# =====================================================================//
-# GITHUB COMPATIBILITY SETTINGS
-# =====================================================================//
-# Github API: https://www.github.com/settings/tokens
-#
-# Check off Options:
-# - 
-
-$GITHUB_TOKEN = "My-Token"
+# API: https://console.curseforge.com/?#/api-keys
+$CURSEFORGE_API_TOKEN = "My-Token"
